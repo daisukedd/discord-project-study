b941b87657a0f505cab3e0387f7f0b5f8d6a9ef1
Kevin "d415uk3" Wallen <46876927+daisukedd@users.noreply.github.com>
2021-10-13 13:48:59 -0300

    add config

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -8,6 +8,15 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+//Comandos
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
 });
 
 // Login to Discord with your client's token
